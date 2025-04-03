579738ab850fd36f4a20523f74af250420169cfb
DwGr <dwarfgrakula@gmail.comm>
2025-04-03 21:27:16 +0400

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar>
+      <NuxtImg
+        class="ml-4"
+        src="/logo.svg"
+        :placeholder="[130, 30]"
+        densities="x1"
+      />
+    </v-app-bar>
+
+    <div class="mt-8">
+      <slot />
+    </div>
+  </div>
+</template>
