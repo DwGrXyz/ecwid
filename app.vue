f492b813f707dbc334edc68a3fe308835f99fcab
DwGr <dwarfgrakula@gmail.comm>
2025-04-03 14:43:52 +0400

    Add data fetching

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+  const config = useRuntimeConfig()
+  const {storeId, token} = config.public
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`},
+
+  baseURL: `https://app.ecwid.com/api/v3/${storeId}`
+  }
+const { data } = await useFetch('/categories', options)
+</script>
+
 <template>
   <div>
+    LEn = {{ data?.total }}
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
+  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
+  runtimeConfig: {
+    public: {
+      token: process.env.NUXT_API_TOKEN,
+      storeId: process.env.NUXT_API_STORE_ID,
+    }
+  }
 })
