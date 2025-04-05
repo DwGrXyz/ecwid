c4da31f28e2dd038247fbbbec9c66c08bc026a0a
DwGr <dwarfgrakula@gmail.comm>
2025-04-05 18:58:22 +0400

    Add tooltip to AppProductBuyButton

--- components/AppProductBuyButton.vue
+++ components/AppProductBuyButton.vue
@@ -2,10 +2,38 @@
 const props = defineProps<{
   id: number;
 }>();
+
+const tooltipShown = ref(false);
+const tooltipTimeout = ref();
+const hideDelay = 1000;
+const clearTooltipTimeout = () => clearTimeout(tooltipTimeout.value);
+const showTooltip = () => {
+  tooltipShown.value = true;
+  clearTooltipTimeout();
+  tooltipTimeout.value = setTimeout(
+    () => (tooltipShown.value = false),
+    hideDelay
+  );
+};
+onBeforeUnmount(clearTooltipTimeout);
+
 const cartStore = useCartStore();
-const addToCart = () => cartStore.addProduct(props.id);
+const addToCart = () => {
+  cartStore.addProduct(props.id);
+  showTooltip();
+};
 </script>
 
 <template>
-  <v-btn color="success" @click="addToCart">Buy</v-btn>
+  <div class="position-relative">
+    <v-tooltip v-model="tooltipShown" :open-on-hover="false">
+      <template v-slot:activator="{ props }">
+        <v-btn color="success" v-bind="props" @click.prevent="addToCart">
+          <span>Buy</span>
+        </v-btn>
+      </template>
+
+      <span>Added!</span>
+    </v-tooltip>
+  </div>
 </template>
